1ec3ebd93d8b54d53ca29cd776cdb2130af5b5c3
Lucas-allard <allard.lucas.97@gmail.com>
2022-12-13 14:39:07 +0100

    post: refacto PostController index + phpdoc

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,22 +5,28 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
+    /**
+     * @param PostRepository $postRepository
+     * @return Response
+     */
     #[Route('/', name: 'home')]
     public function index(PostRepository $postRepository): Response
     {
-        $posts = $postRepository->findAll();
 
         return $this->render('home/index.html.twig', [
-            'posts' => $posts
+            'posts' => $postRepository->findAll()
         ]);
     }
 
+    /**
+     * @param Post $post
+     * @return Response
+     */
     #[Route('/post/{id}', name: 'post')]
     public function show(Post $post): Response
     {
